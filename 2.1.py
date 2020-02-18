4debedea627e53f9530028e55c3213ed1099413a
Zaarib <zz3553@g.rit.edu>
2020-02-18 02:11:34 -0500

    implemented add functions with minimal testing

--- 2.1.py
+++ 2.1.py
@@ -23,8 +23,14 @@ def deletionByData(lst, val):
 
 #return 0 -> not deleted, 1 -> deleted
 def deletionAtIndex(lst, index):
+
+    if index == 0:
+        lst = lst.next
+        return 1
+
     count = 0
     temp = lst
+
     while ( count != index ):
         count += 1
         if ( count == index ):
@@ -34,13 +40,40 @@ def deletionAtIndex(lst, index):
             temp = temp.next
     return 0
 
-def addAt(lst, index):
+
+#
+def addAt(lst, index, val):
     count = 0 
+    temp = lst
+    while ( temp is not None ):
+        count += 1
+        if count == index:
+            new = Node(val, temp.next)
+            temp.next = new
+            return 1
+        else:
+            temp = temp.next
+
+    return 0
+
+def add(lst, val):
+    temp = lst
+    while ( temp is not None ):
+        temp = temp.next
+        if temp.next is None:
+            temp.next = Node(val, None)
+            return 1
+    
+    return 0
 
 def main():
     lst = Node(7, None)
     lst2 = Node(6, lst)
-    
+    # print(deletionAtIndex(lst2, 1))
+
+    print(addAt(lst2, 1, 1))
+
+    print(add(lst2, 3))
 
 
 main()
