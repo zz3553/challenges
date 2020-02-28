138c4cbf5590da6e70c9bd364737ed37b17710b9
Zaarib <zz3553@g.rit.edu>
2020-02-27 23:54:32 -0500

    done with 2.2

--- 2.2.py
+++ 2.2.py
@@ -12,13 +12,13 @@ def solution(lst, k):
         temp = temp.next
     
     i = 0
-    count = len - k
-    while i != count:
-        i += 1
-        if i == k:
-            return lst.next.data
+    count = len - k 
+    while i != len: #goes through length of list
+        if i == count: #if i is up to kth element
+            return lst.data
         else:
             lst = lst.next
+        i += 1
 
     return 0    
 
@@ -27,6 +27,6 @@ def main():
     lst2 = Node(2, lst)
     lst3 = Node(1, lst2)
     lst4 = Node(2, lst3)
-    print(solution(lst4, 2))
+    print(solution(lst4, 4))
 
 main()
