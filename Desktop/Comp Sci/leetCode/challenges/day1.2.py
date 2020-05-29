b0e2ba92481a647b61d28e38ab86040c14019cea
Zaarib <zz3553@g.rit.edu>
2020-05-28 22:34:23 -0400

    Implemented second solution with hashmap -> o(n)

--- Desktop/Comp Sci/leetCode/challenges/day1.2.py
+++ Desktop/Comp Sci/leetCode/challenges/day1.2.py
@@ -2,10 +2,31 @@
 
 
 def solve(str1, str2):
-    str1 = sorted(str1)
-    str2 = sorted(str2)
-    if str1 == str2: 
-        return True
-    return False
+    # str1 = sorted(str1)   easy solution by sorting both strings 
+    # str2 = sorted(str2)
+    # if str1 == str2: 
+    #     return True
+    # return False
 
-print(solve("rac", "car"))
+    map = {}
+
+    if len(str1) != len(str2): #base case
+        return False
+
+    for i in str1:
+        if i not in map:
+            map[i] = 1
+        else:
+            map[i] += 1
+    
+    for i in str2:
+        if i in map and map[i] > 0:
+            map[i] -= 1
+        elif i in map and map[i] < 1:
+            return False
+        elif i not in map:
+            return False
+    
+    return True
+
+print(solve("hero", "rehr"))
