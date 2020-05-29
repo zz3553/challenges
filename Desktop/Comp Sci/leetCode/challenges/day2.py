551d66f60a20e7c277d78e6b386d770ef73cf8ba
Zaarib <zz3553@g.rit.edu>
2020-05-29 12:31:45 -0400

    Implmented solution with splitting into list

--- Desktop/Comp Sci/leetCode/challenges/day2.py
+++ Desktop/Comp Sci/leetCode/challenges/day2.py
@@ -6,10 +6,21 @@
 
 
 # EXAMPLE
-# Input: "Mr 3ohn Smith 13 
+# Input: "Mr 3ohn Smith, 13 
 # Output: "Mr%203ohn%20Smith"
 
-def solve(str):
-    return None
+STR = '%20' #replace space with this 
+
+def solve(str, size):
+    # str = str.split()   solution with splitting into list 
+    # for i in range(len(str)):
+    #     if i != len(str)-1 and str[i] != STR: #not end of list
+    #         str.insert(i+1, STR)
+
+    # return str
+
+
+
+print(solve("Mr John    Smith   ", 13))
 
 
