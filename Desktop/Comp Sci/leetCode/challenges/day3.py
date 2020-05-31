c261af76934f451cd808b4486c3db2c98b6a0b8c
Zaarib <zz3553@g.rit.edu>
2020-05-31 11:48:32 -0400

    Solved and showed off few tests

--- Desktop/Comp Sci/leetCode/challenges/day3.py
+++ Desktop/Comp Sci/leetCode/challenges/day3.py
@@ -10,17 +10,29 @@
 # Output: True -> ("taco cat", "atco eta", ... )
 
 def solve(str):
-    map = {}
+    """
+    if len is even -> palindrome will have each letter appear twice, odd -> one letter appears once
+    order doesnt matter since they could re arranged 
+    """
+    map = {} #keeps track of coutns of letter 
     for i in str:
         if i not in map:
             map[i] = 1
         else:
             map[i] += 1
 
+    ones = 0
     for i in map:
         if map[i] > 2:
             return False
+        if map[i] == 1 and i != ' ':
+            ones += 1
 
+    if ones > 1:
+        return False
     return True
 
-print(solve("tact coa"))
+
+tests = ['tact coa', 'abba', 'abbc']
+for i in tests:
+    print(solve(i))
