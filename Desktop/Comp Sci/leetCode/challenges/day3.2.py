4ada6f740364f4f1831b00cb0b1abb85a6b864bd
Zaarib <zz3553@g.rit.edu>
2020-05-31 13:11:57 -0400

    3 separate loops for implementing

--- Desktop/Comp Sci/leetCode/challenges/day3.2.py
+++ Desktop/Comp Sci/leetCode/challenges/day3.2.py
@@ -3,10 +3,47 @@
 # Given two strings, write a function to check if they are one edit (or zero edits) away.
 
 # EXAMPLE
-# pale, pie -> true
+# pale, pie -> false        
 # pales, pale -> true
 # pale, bale -> true
 # pale, bake -> false
+# pale, pales -> true
 
-def solve(str):
-    return False
+import string
+
+def solve(str, str2):
+    if str == str2: return True #if theyre the same 
+
+    str = str.lower()
+    str2 = str2.lower()
+    str = list(str)
+    for  i in range(len(str)+1):  # inserting character 
+        str2 = list(str2)
+        for x in range(len(str2)):
+            str.insert(i, str2[x])
+            if "".join(str) == "".join(str2):
+                return True
+            str.pop(i)
+
+    for i in range(len(str)):  # removing character 
+        popped = str.pop(i)
+        if "".join(str) == str2:
+            return True
+        str.insert(i, popped)
+
+    for i in range(len(str)): #swapping 
+        popped = str.pop(i)
+        str2 = list(str2)
+        for x in range(len(str2)):
+            str.insert(i, str2[x])
+            if "".join(str) == "".join(str2):
+                return True
+            str.pop(i)
+        str.insert(i, popped)
+    
+
+
+
+            
+
+print(solve('sfdsf', 'afdsf'))
