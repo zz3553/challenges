a78154566cb353d0f9dcc3e6addda535720ef4be
Zaarib <zz3553@g.rit.edu>
2020-05-31 11:21:46 -0400

    Added problem

---
+++ Desktop/Comp Sci/leetCode/challenges/day3.py
@@ -0,0 +1,14 @@
+# Palindrome Permutation: Given a string, write a function to check if it 
+# is a permutation of a palin- drome. 
+# A palindrome is a word or phrase that is the same forwards and backwards. 
+# A permutation is a rearrangement of letters. 
+# The palindrome does not need to be limited to just dictionary words.
+ 
+# EXAMPLE
+
+# Input : Tact Coa
+# Output: True -> ("taco cat", "atco eta", ... )
+
+def solve(str):
+    return False
+
