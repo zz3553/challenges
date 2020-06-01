dd6d55a2547ded46f4d8ec53f18e25d02cb2fb0c
Zaarib <zz3553@g.rit.edu>
2020-06-01 14:09:24 -0400

    sort of brute force, using extra array, also resulting array is flipped upside down

--- Desktop/Comp Sci/leetCode/challenges/day4.py
+++ Desktop/Comp Sci/leetCode/challenges/day4.py
@@ -4,4 +4,30 @@
 # (Can you do this in place?)
 
 def solve(image):
+    result = [ [ 0 for i in range(len(image)) ] for j in range(len(image)) ]
+    index = len(result)-1
+    index2 = len(result)-1 
+    for i in range(len(image)):
+        map = image[i]
+        for x in range(len(map)):
+            result[index2][index] = map[x]
+            index2 -= 1
+        index -= 1
+        index2 = len(result)-1
+    
+    print(result)
     return None
+
+matrix = [[0,1],
+          [2,3]]
+
+matrix2 = [[0,1,2],
+            [3,4,5],
+                [6,7,8]]
+
+matrix3 = [[0,1,2,3],
+            [4,5,6,7],
+                [8,9,10,11],
+                    [12,13,14,15]]
+
+solve(matrix3)
