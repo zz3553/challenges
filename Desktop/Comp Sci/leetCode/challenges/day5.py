c2a4dba605decd6e1fba2500c5eff65e41467b4f
Zaarib <zz3553@g.rit.edu>
2020-06-02 18:44:45 -0400

    Need to reverse the list

--- Desktop/Comp Sci/leetCode/challenges/day5.py
+++ Desktop/Comp Sci/leetCode/challenges/day5.py
@@ -4,10 +4,11 @@
 # Write a function that adds the two numbers and returns the sum as a linked list.
 
 # EXAMPLE
-# Input: (7-) 1 -) 6) + (5 -) 9 -) 2).Thatis,617 + 295. Output: 2 -) 1 -) 9.That is, 912.
+# Input: (7-> 1 -> 6) + (5 -> 9 -> 2). Thatis,617 + 295. Output: 2 -> 1 -> 9.That is, 912.
+
 # FOLLOW UP
 # Suppose the digits are stored in forward order. Repeat the above problem. EXAMPLE
-# Input: (6 -) 1 -) 7) + (2 -) 9 -) 5).Thatis,617 + 295. Output: 9 -) 1 -) 2.That is, 912.
+# Input: (6 -> 1 -> 7) + (2 -> 9 -> 5).That is,617 + 295. Output: 9 -> 1 -> 2.That is, 912.
 
 from dataclasses import dataclass
 
@@ -18,4 +19,37 @@ class Node:
     self.next = next
 
 def solve(list1, list2):
-    return 0
+    sum = 0
+    tens = 0
+    while list1 is not None:
+        sum += list1.data * (10**tens)
+        tens+=1
+        list1 = list1.next
+    
+    tens = 0
+    while list2 is not None:
+        sum += list2.data * (10**tens)
+        tens+=1
+        list2 = list2.next
+
+    someNode = Node(sum%10, None)
+    sum = sum // 10
+    a = sum % 10
+    while sum is not 0:
+        someNode = helper(someNode, sum%10)
+        sum = sum // 10
+
+    return someNode
+
+def helper(node, val):
+    if val//10 == 0 and val %10 != 0:
+        node2 = Node(val, node)
+        return node2
+    return
+
+
+
+node1 = Node(6, None)
+node2 = Node(1, node1)
+node3 = Node(7, node2)
+print(solve(node3, node3))
